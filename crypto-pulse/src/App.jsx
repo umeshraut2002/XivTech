cb8ee89706f085c629b15bfd396b20678793dfe6
umesh raut <uraut6044@gmail.com>
2025-04-24 23:00:20 +0530

    coins page home page route created

--- crypto-pulse/src/App.jsx
+++ crypto-pulse/src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Header from './components/Header';
 import './App.css'
+import Homepage from './pages/Homepage';
+import Coinpage from './pages/Coinpage';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header/>
-        <Route path='' Component={} />
+        <Route path='/' Component={Homepage} exact />
+        <Route path='/coin/id' Component={Coinpage} />
       </div>
     </BrowserRouter>
   )
