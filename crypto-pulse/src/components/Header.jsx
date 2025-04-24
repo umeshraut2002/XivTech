ae1aa4fb2fa04a8cf64ba1109f9e92557cb37e7a
umesh raut <uraut6044@gmail.com>
2025-04-24 23:57:39 +0530

    header created

--- crypto-pulse/src/components/Header.jsx
+++ crypto-pulse/src/components/Header.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { AppBar, Toolbar, Typography, Container } from '@mui/material';
 
 const Header = () => {
   return (
-    <div>
-    <h1>Header</h1>
-    </div>
-  )
-}
+    <AppBar position="static" style={{ backgroundColor: '#14161a' }}>
+      <Container>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            style={{
+              flex: 1,
+              color: 'orange',
+              fontFamily: 'Montserrat',
+              fontWeight: 'bold',
+              cursor: 'pointer',
+            }}
+          >
+            Crypto Tracker
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 
 export default Header;
